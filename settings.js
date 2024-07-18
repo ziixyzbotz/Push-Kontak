723f8ce6337b3e5637bc83aeb9f8d244c99a0605
ziixyzbotz <stockramzi3@gmail.com>
2024-07-18 20:34:27 +0000

    Pending changes exported from your codespace

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
-global.owner = ["62887435047326"]
+global.owner = ["628886250672"]
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
-global.pairingNumber = ""
+global.pairingNumber = "628886250672"
